f19110ff87e8e32c0dda1fc36904bde74bb203af
kagorax <kelan360@gmail.com>
2022-05-13 17:21:22 +0100

    New Repo for HelloWorld

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String... args){
+       int x = 0;
+       int y= 10;
+
+       if (x == y){
+           System.out.println("1");
+        } else if (x>y) {
+           System.out.println("2");
+       }
+       else System.out.println("3");
+    }
+}
